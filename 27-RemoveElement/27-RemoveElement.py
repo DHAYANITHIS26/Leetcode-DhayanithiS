71ef044c833eda22b121e540ff9f070efe8410e1
DHAYANITHI S <sdhayanithi70@gmail.com>
2026-08-11 16:07:08 +0530

    Create 27-RemoveElement

---
+++ 27-RemoveElement/27-RemoveElement.py
@@ -0,0 +1,11 @@
+# Last updated: 11/08/2026, 16:07:07
+class Solution(object):
+    def removeElement(self, nums, val):
+        i = 0
+        
+        for num in nums:
+            if num != val:
+                nums[i] = num
+                i += 1
+        
+        return i
