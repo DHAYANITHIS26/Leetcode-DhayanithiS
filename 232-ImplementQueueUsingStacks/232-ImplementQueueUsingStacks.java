f6022d1c2e19136d3ab2a97159cccdf1e16cd72f
DHAYANITHI S <sdhayanithi70@gmail.com>
2026-08-11 16:03:50 +0530

    Create 232-ImplementQueueUsingStacks

---
+++ 232-ImplementQueueUsingStacks/232-ImplementQueueUsingStacks.java
@@ -0,0 +1,41 @@
+// Last updated: 11/08/2026, 16:03:49
+class MyQueue {
+    private Stack<Integer> first;
+    private Stack<Integer> second;
+    public MyQueue() {
+        first = new Stack<>();
+        second = new Stack<>();
+    }
+    public void push(int x) {
+        first.push(x);
+    }
+    public int pop() {
+        while (!first.isEmpty()) {
+            second.push(first.pop());
+        }
+
+        int firstElement = second.pop();
+
+        while (!second.isEmpty()) {
+            first.push(second.pop());
+        }
+
+        return firstElement;
+    }
+    public int peek() {
+        while (!first.isEmpty()) {
+            second.push(first.pop());
+        }
+
+        int firstElement = second.peek();
+
+        while (!second.isEmpty()) {
+            first.push(second.pop());
+        }
+
+        return firstElement;
+    }
+    public boolean empty() {
+        return first.isEmpty();
+    }
+}
